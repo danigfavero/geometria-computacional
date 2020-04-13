eeb3b9ced45b185b96626e8fd198a87df86f2c51
danigfavero <danifavero@usp.br>
2020-04-13 13:32:45 -0300

    Sets up platform for second project

---
+++ geocomp-py-framework/geocomp/lineintersections/bentley_ottmann.py
@@ -0,0 +1,10 @@
+from geocomp.common import prim
+from geocomp.common import segment
+from geocomp.common import control
+from geocomp import config
+
+def Bentley_Ottmann(l):
+    ''' Recebe uma coleção de segmentos e devolve o número de interseções
+    presente na coleção
+    '''
+    pass
