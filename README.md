fdb19b35a7fe773770062d4bddebe9e904fe207e
danigfavero <danifavero@usp.br>
2020-04-16 11:24:48 -0300

    Progresso no EP2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projetos Geometria Computacional
+
+1. Triangulação de polígonos
+
+2. Detecção de todas as intersecções numa coleção de segmentos
--- geocomp-py-framework/geocomp/lineintersections/bentley_ottmann.py
+++ geocomp-py-framework/geocomp/lineintersections/bentley_ottmann.py
@@ -4,6 +4,17 @@ from geocomp.common import control
 from geocomp import config
 from bintrees import AVLTree
 
+class Event:
+    "Ponto evento da linha de varredura"
+    def __init__(self, t, *args):
+        self.t = t
+        if type == "left" or type == "right":
+            self.s = list(args)[0]
+        if type == "inter":
+            self.s1 = list(args)[0]
+            self.s2 = list(args)[1]
+
+
 def Bentley_Ottmann(l):
     ''' Recebe uma coleção de segmentos e devolve o número de interseções
     presente na coleção
@@ -14,56 +25,80 @@ def Bentley_Ottmann(l):
 def sorted_extremes(l, n):
     ''' Ordena os extremos dos n segmentos da lista l
     '''
-    q = AVLTree()
+    Q = AVLTree()
     for s in l:
         if s.upper == s.lower:
             pass # TODO corner case!!!
-        q[s.upper] = s
-        q[s.lower] = s
-    return q
+        Q[s.upper] = Event("left", s)
+        Q[s.lower] = Event("right", s)
+    return Q
 
 def find_intersections(l, n):
-    q = sorted_extremes(l, n)
-    t = AVLTree()
-    while not q.is_empty():
-        p = q.pop_min()
-        treat_event(p, q, t)
+    Q = sorted_extremes(l, n)
+    T = AVLTree()
+    while not Q.is_empty():
+        p = Q.pop_min()
+        treat_event(p, Q, T)
 
-def treat_event(p, q, t):
-    s = p[1]
-    e = p[0]
+def treat_event(p, Q, T):
+    event = p[1]
+    point = p[0]
 
-    if e == s.upper:
-        # t.insert(s) TODO oxe mas é uma symboltable, qual é a key?
-        # não esquece de passar essa chave pra "e" ou renomeie isso
+    if event.t == "left":
+        s = event.s
+        # T.insert(s) TODO oxe mas é uma symboltable, Qual é a key?
+        # não esquece de passar essa chave pra "point" ou renomeie isso
         # usaremos essa chave pra achar o intersects
         try:
-            pred = t.prev_item(e)
+            pred = T.prev_item(point)
             if s.intersects(pred):
-                verify_new_event(p, q, s, pred)
+                verify_new_event(p, Q, s, pred)
         except KeyError:
             pass
         try:
-            succ = t.succ_item(e)
+            succ = T.succ_item(point)
             if s.intersects(succ):
-                verify_new_event(p, q, s, succ)
+                verify_new_event(p, Q, s, succ)
         except KeyError:
             pass
 
-    if e == s.lower:
+    if event.t == "right":
+        s = event.s
         try:
-            pred = t.prev_item(e)
-            succ = t.succ_item(e)
+            pred = T.prev_item(point)
+            succ = T.succ_item(point)
             if pred.intersects(succ):
-                verify_new_event(p, q, succ, pred)
+                verify_new_event(p, Q, succ, pred)
         except KeyError:
             pass
 
-    if False: # TODO se p é ponto de intersecção
-        pass
+    if event.t == "inter":
+        s1 = event.s1
+        s2 = event.s2
+        try:
+            pred = T.pred_item(s1)
+        except KeyError:
+            pred = None
+        try:
+            succ = T.succ_item(s2)
+        except KeyError:
+            succ = None
+        T.pop(s1)
+        T.pop(s2)
+        # T.insert(s2)
+        # T.insert(s1)
+        # na ordem inversa TODO
+        if pred != None and pred.intersects(s2):
+            verify_new_event(p, Q, pred, s2)
+        if succ != None and s1.intersects(succ):
+            verify_new_event(p, Q, s1, succ)
 
- 
+def verify_new_event(p, Q, s1, s2):
+    q = intersection_point(s1, s2)
+    if q > p and q not in Q:
+        Q[q] = Event("inter", s1, s2)
+        print(q)
 
-def verify_new_event(p, q, s, t):
-    # TODO pensar em todo esse caso de pto de intersecção
+def intersection_point(s1, s2):
+    # TODO como eu sei qual é o (primeiro) ponto no qual eles se intersectam
     pass
