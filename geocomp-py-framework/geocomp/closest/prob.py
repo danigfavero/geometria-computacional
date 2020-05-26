ddcc724fbc241ee6d84d18c1350bb5957ec28ff3
bentopereira <bento.pereira@usp.br>
2020-05-26 19:15:22 -0300

    corrigida animacao

--- geocomp-py-framework/geocomp/closest/prob.py
+++ geocomp-py-framework/geocomp/closest/prob.py
@@ -8,9 +8,9 @@ import math
 from random import shuffle
 
 
-def square_id(p, delta):
+def square_id(p, delta, off):
 	plc = math.sqrt(delta)
-	return (math.floor((2*p.x)/plc), math.floor((2*p.y)/plc))
+	return (math.floor((2*(p.x + off))/plc), math.floor((2*(p.y+off))/plc))
 
 
 def Prob (l):
@@ -23,27 +23,25 @@ def Prob (l):
 
 	for p in l:
 		offset = min(offset, p.x, p.y)
-
-	nl = []
-	if offset < 0:
-		for i in range(0, len(l)):
-			plc = l[i]
-			plc.x = plc.x - offset
-			plc.y = plc.y - offset
-			nl.append(plc)
-	else:
-		nl = l 
+	
+	offset = min(0, offset)
 	
 
 	delt = dist2(l[0], l[1])
 	S = {}
-	S[square_id(nl[0], delt)] = 0
-	S[square_id(nl[1], delt)] = 1
-	a = b = None
-	id = None
+	S[square_id(l[0], delt, offset)] = 0
+	S[square_id(l[1], delt, offset)] = 1
+	a = l[0]
+	b = l[1]
+	control.freeze_update ()
+	hia = a.hilight()
+	hib = b.hilight()
+	id = a.lineto (b)
+	control.thaw_update() 
+	control.update ()
 
 	for i in range (2, len(l)):
-		cur_id = square_id(nl[i], delt)
+		cur_id = square_id(l[i], delt, offset)
 		updt = False
 		for u in range(-2, 3):
 			for v in range(-2, 3):
@@ -66,18 +64,18 @@ def Prob (l):
 						id = a.lineto (b)
 						control.thaw_update() 
 						control.update ()
-						
+		if delt == 0:
+			break
 		if updt:
 			S.clear()
 			for j in range(0, i + 1):
-				S[square_id(nl[j], delt)] = j
+				S[square_id(l[j], delt, offset)] = j
 		else:
 			S[cur_id] = i
-
-
 
 	a.hilight('green')
 	b.hilight('green')
 	ret = Segment (a, b)
 	ret.extra_info = 'distancia: %.2f'%math.sqrt (dist2 (a, b))
+	S.clear()
 	return ret
