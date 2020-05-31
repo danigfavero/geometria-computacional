dd7237d77817e4860a0bc147ca964b6b4fb600ac
danigfavero <danifavero@usp.br>
2020-05-31 02:06:35 -0300

    o que ta acontecendo AAAA

--- geocomp-py-framework/geocomp/lineintersections/my_tree.py
+++ geocomp-py-framework/geocomp/lineintersections/my_tree.py
@@ -56,6 +56,75 @@ class MyTree(RBTree):
         self._root = head.right  # Update root
         self._root.red = False  # make root black
 
+    def remove(self, key):
+        """T.remove(key) <==> del T[key], remove item <key> from tree.
+        Comparações feitas por valor"""
+        if self._root is None:
+            raise KeyError(str(key))
+        head = Node()  # False tree root
+        node = head
+        node.right = self._root
+        parent = None
+        grand_parent = None
+        found = None  # Found item
+        direction = 1
+        value = self[key]
+
+        # Search and push a red down
+        while node[direction] is not None:
+            last = direction
+
+            # Update helpers
+            grand_parent = parent
+            parent = node
+            node = node[direction]
+
+            direction = 1 if value > node.value else 0
+
+            # Save found node
+            if value == node.value:
+                found = node
+
+            # Push the red node down
+            if not is_red(node) and not is_red(node[direction]):
+                if is_red(node[1 - direction]):
+                    parent[last] = jsw_single(node, direction)
+                    parent = parent[last]
+                elif not is_red(node[1 - direction]):
+                    sibling = parent[1 - last]
+                    if sibling is not None:
+                        if (not is_red(sibling[1 - last])) and (not is_red(sibling[last])):
+                            # Color flip
+                            parent.red = False
+                            sibling.red = True
+                            node.red = True
+                        else:
+                            direction2 = 1 if grand_parent.right is parent else 0
+                            if is_red(sibling[last]):
+                                grand_parent[direction2] = jsw_double(parent, last)
+                            elif is_red(sibling[1 - last]):
+                                grand_parent[direction2] = jsw_single(parent, last)
+                            # Ensure correct coloring
+                            grand_parent[direction2].red = True
+                            node.red = True
+                            grand_parent[direction2].left.red = False
+                            grand_parent[direction2].right.red = False
+
+        # Replace and remove if found
+        if found is not None:
+            found.value = node.value
+            found.key = node.key
+            parent[int(parent.right is node)] = node[int(node.left is None)]
+            node.free()
+            self._count -= 1
+
+        # Update root and make it black
+        self._root = head.right
+        if self._root is not None:
+            self._root.red = False
+        if not found:
+            raise KeyError(str(key))
+
 if __name__ == "__main__":
     t = MyTree()
     t[1] = 2
